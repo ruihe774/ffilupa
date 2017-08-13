937673bc8b9646a8dd766dc71a873178c0f86d1d
TitanSnow <sweeto@live.cn>
2017-08-13 21:32:43 +0800

    run on py2

--- demo_repl.py
+++ demo_repl.py
@@ -1,5 +1,6 @@
 import sys
 import readline
+import six
 import ffilupa.lua as lua
 
 
@@ -8,11 +9,11 @@ lua.lib.luaL_openlibs(L)
 
 try:
     while True:
-        buff = input('>>> ').encode()
+        buff = six.moves.input('>>> ').encode()
         error = bool(lua.lib.luaL_loadbuffer(L, buff, len(buff), b"line")) or \
                 bool(lua.lib.lua_pcall(L, 0, 0, 0))
         if error:
-            print(lua.ffi.string(lua.lib.lua_tostring(L, -1)).decode(), file=sys.stderr)
+            six.print_(lua.ffi.string(lua.lib.lua_tostring(L, -1)).decode(), file=sys.stderr)
             lua.lib.lua_pop(L, 1)
 except EOFError:
     lua.lib.lua_close(L)
