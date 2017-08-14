e0e883af8468c390650c07331f4ae55a2d426289
TitanSnow <sweeto@live.cn>
2017-08-14 14:04:23 +0800

    add packages

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='ffilupa',
+    packages=['ffilupa'],
     setup_requires=["cffi>=1.0.0"],
     cffi_modules=["ffibuilder_lua.py:ffibuilder"],
     install_requires=["cffi>=1.0.0", "six>=1.9.0"],
