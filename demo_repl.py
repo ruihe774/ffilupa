8025c0b967e4ddd657997878d4177fbb4466f188
TitanSnow <sweeto@live.cn>
2017-08-17 21:31:35 +0800

    multiline repl

--- demo_repl.py
+++ demo_repl.py
@@ -1,19 +1,77 @@
+#!/usr/bin/env python
+
 import sys
 import readline
 import six
-import ffilupa.lua as lua
+import ffilupa
+
+lua = ffilupa.lua
+ll = lua.lib
+lf = lua.ffi
 
 
-L = lua.lib.luaL_newstate()
-lua.lib.luaL_openlibs(L)
+class LuaREPL(object):
+    def __init__(self):
+        self.lua_runtime = ffilupa.LuaRuntime()
 
-try:
-    while True:
-        buff = six.moves.input('>>> ').encode()
-        error = bool(lua.lib.luaL_loadbuffer(L, buff, len(buff), b"line")) or \
-                bool(lua.lib.lua_pcall(L, 0, 0, 0))
-        if error:
-            six.print_(lua.ffi.string(lua.lib.lua_tostring(L, -1)).decode(), file=sys.stderr)
-            lua.lib.lua_pop(L, 1)
-except EOFError:
-    lua.lib.lua_close(L)
+    PROMPT1 = '>>> '
+    PROMPT2 = '... '
+    EOFMARK	= b'<eof>'
+
+    def readline(self, firstline):
+        return six.moves.input(self.PROMPT1 if firstline else self.PROMPT2)
+
+    def testline(self, buffer):
+        L = self.lua_runtime.lua_state
+        buf = buffer.encode()
+        status = ll.luaL_loadbuffer(L, buf, len(buf), b'=stdin')
+        return status
+
+    def incomplete(self, buffer):
+        L = self.lua_runtime.lua_state
+        status = self.testline(buffer)
+        if status == ll.LUA_ERRSYNTAX:
+            if lf.string(ll.lua_tostring(L, -1)).endswith(self.EOFMARK):
+                ll.lua_pop(L, 1)
+                return True
+        return False
+
+    def loadline(self):
+        L = self.lua_runtime.lua_state
+        lines = []
+        firstline = True
+        def pushline():
+            lines.append(self.readline(firstline))
+        pushline()
+        firstline = False
+        if self.testline('return {};'.format(lines[0])) != ll.LUA_OK:
+            ll.lua_pop(L, 1)
+            while self.incomplete('\n'.join(lines)):
+                pushline()
+
+    def docall(self):
+        L = self.lua_runtime.lua_state
+        return ll.lua_pcall(L, 0, ll.LUA_MULTRET, 0)
+
+    def print_result(self):
+        L = self.lua_runtime.lua_state
+        n = ll.lua_gettop(L)
+        if n > 0:
+            ll.lua_getglobal(L, b'print')
+            ll.lua_insert(L, 1)
+            ll.lua_pcall(L, n, 0, 0)
+
+    def dorepl(self):
+        L = self.lua_runtime.lua_state
+        try:
+            while True:
+                self.loadline()
+                if self.docall() == ll.LUA_OK:
+                    self.print_result()
+        except EOFError:
+            pass
+
+
+if __name__ == '__main__':
+    repl = LuaREPL()
+    repl.dorepl()
