e74390606e19c5f389b9c1abab8718f79894ce20
TitanSnow <sweeto@live.cn>
2017-08-18 14:18:00 +0800

    prepare for release
    Squashed commit of the following:
    
    commit 6785e72190286e41e5b6dd792df257eb0f487af0
    Author: TitanSnow <sweeto@live.cn>
    Date:   Fri Aug 18 13:52:46 2017 +0800
    
        add MANIFEST.in
    
    commit 07ad628e91734180639f12ed33665929f4d2e044
    Author: TitanSnow <sweeto@live.cn>
    Date:   Fri Aug 18 11:51:36 2017 +0800
    
        seperator version
    
    commit 013b2d9cc3269f1571219c735681f90ef5684e0d
    Author: TitanSnow <sweeto@live.cn>
    Date:   Fri Aug 18 11:29:06 2017 +0800
    
        add CI scripts
    
    commit 6892511d3443aecafb99980fec335de9b62a2d6d
    Author: TitanSnow <sweeto@live.cn>
    Date:   Fri Aug 18 11:25:32 2017 +0800
    
        add more info in setup.py
    
    commit 6c3f5be87aaad9009df95b66c441a7d976d7d20a
    Author: TitanSnow <sweeto@live.cn>
    Date:   Fri Aug 18 11:09:16 2017 +0800
    
        add README.rst
    
    commit 6a325eb587f9eb4b31ddcbb1066b503f8c42d63c
    Author: TitanSnow <sweeto@live.cn>
    Date:   Fri Aug 18 10:17:21 2017 +0800
    
        add __version__
    
    commit edd0d686859aaffcd733d1d3effb9f276a9cb209
    Author: TitanSnow <sweeto@live.cn>
    Date:   Fri Aug 18 10:14:16 2017 +0800
    
        remove assert

--- setup.py
+++ setup.py
@@ -1,10 +1,37 @@
 from setuptools import setup
+import runpy
+
+VERSION = runpy.run_path('ffilupa/version.py')['__version__']
 
 setup(
     name='ffilupa',
+    version=VERSION,
+    author="TitanSnow",
+    author_email="tttnns1024@gmail.com",
+    url='https://github.com/TitanSnow/lupa/tree/ffi',
+    description='cffi implement of lupa with lowlevel lua API',
+    long_description=open('README.rst').read(),
+    license='MIT style',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Other Scripting Engines',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+    ],
     packages=['ffilupa'],
     setup_requires=["cffi>=1.10.0"],
     cffi_modules=["ffibuilder_lua.py:ffibuilder"],
     install_requires=["cffi>=1.10.0", "six>=1.9.0"],
     test_suite='ffilupa.tests.suite',
+    zip_safe=True,
 )
