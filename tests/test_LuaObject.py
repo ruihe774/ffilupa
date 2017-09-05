113a9c7e127a751eb9f80e519cd978ec7887c566
TitanSnow <sweeto@live.cn>
2017-09-05 19:03:38 +0800

    add more tests

--- tests/test_LuaObject.py
+++ tests/test_LuaObject.py
@@ -3,7 +3,7 @@ import six
 import pytest
 from ffilupa import *
 from ffilupa.py_from_lua import LuaObject
-from ffilupa.py_to_lua import push
+from ffilupa.py_to_lua import push, push_pyobj
 from ffilupa.util import ensure_stack_balance, lock_get_state
 
 
@@ -81,6 +81,11 @@ def test_table_get():
     assert rt.eval('{awd="dwa"}')[b'awd'] == b'dwa'
     assert rt.eval('{awd="dwa"}').awd == b'dwa'
     assert rt.eval('{__hash__="dwa"}').__hash__ != b'dwa'
+    tb = rt.eval('{edit_mode="dwa"}')
+    assert tb.edit_mode == False
+    tb.edit_mode = True
+    with pytest.raises(AttributeError):
+        tb.awd
 
 def test_table_set():
     tb = rt.eval('{}')
@@ -107,6 +112,11 @@ def test_table_set():
     assert tb.awd == b'dwa'
     assert tb.__hash__ == 5
     assert tb['__hash__'] == None
+
+    tb.edit_mode = True
+    tb.awd = b'awd'
+    assert tb['awd'] == b'dwa'
+    assert tb.awd == b'awd'
 
 def test_table_add():
     with pytest.raises(LuaErr):
@@ -139,3 +149,36 @@ def test_nil_debug():
     assert lua.eval('1') is 1
     with pytest.raises(LuaErr):
         lua.eval('#awd')
+
+def test_multiple_returnval():
+    assert rt.eval('1, 2') == (1, 2)
+
+def test_table_pull():
+    tb = rt.eval('{1,2,3}')
+    assert tb.pull() == tb
+
+def test_int_pull(loint_a):
+    assert loint_a.pull() == int_a
+    assert type(loint_a.pull()) is type(int_a)
+
+def test_getmetafield():
+    push_pyobj(rt, 1, 1)
+    assert callable(LuaObject(rt, -1).getmetafield('__call'))
+
+def test_typename(loint_a):
+    assert loint_a.typename() == 'number'
+    assert LuaObject.typename(loint_a) == 'number'
+
+def test_repr_cache(loint_a):
+    repr(loint_a)
+
+def test_del_attr_item():
+    tb = rt.eval('{2,3,4,awd="dwa",__add__="ccc"}')
+    del tb[1]
+    assert sorted(tb.items(), key=hash) == sorted(((1, 3), (2, 4), (b'awd', b'dwa'), (b'__add__', b'ccc')), key=hash)
+    del tb.awd
+    assert sorted(tb.items(), key=hash) == sorted(((1, 3), (2, 4), (b'__add__', b'ccc')), key=hash)
+    del tb.__add__
+    assert sorted(tb.items(), key=hash) == sorted(((1, 3), (2, 4), (b'__add__', b'ccc')), key=hash)
+    del tb['__add__']
+    assert sorted(tb.items(), key=hash) == sorted(((1, 3), (2, 4)), key=hash)
