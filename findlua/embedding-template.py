4f470774e68725058950e87a386e7ac8b0ca40d2
TitanSnow <sweeto@live.cn>
2018-04-15 07:45:56 +0800

    install python package as data in lua package

--- findlua/embedding-template.py
+++ findlua/embedding-template.py
@@ -1,6 +1,10 @@
 from {libname} import ffi
-from ffilupa import embedding
 
 @ffi.def_extern()
-def _ffilupa_init(L):
+def _ffilupa_init(L, path):
+    import os
+    path = os.fsdecode(ffi.string(path))
+    import sys
+    sys.path.insert(0, path)
+    from ffilupa import embedding
     embedding.init(L, '{libname}'[19:])
--- findlua/embedding.c
+++ findlua/embedding.c
@@ -1,9 +1,11 @@
 #include <lua.h>
 #include <lauxlib.h>
 
-void _ffilupa_init(lua_State*);
+void _ffilupa_init(lua_State*, const char*);
 
 void ffilupa_init(lua_State* L){
+    const char *path;
     luaL_checkversion(L);
-    _ffilupa_init(L);
+    path = luaL_checkstring(L, 1);
+    _ffilupa_init(L, path);
 }
