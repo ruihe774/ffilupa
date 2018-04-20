73b8bfc2206572061848dfab5e304ea6f4162698
TitanSnow <sweeto@live.cn>
2018-04-21 07:08:29 +0800

    raise errors during loading

--- ffilupa.lua
+++ ffilupa.lua
@@ -2,12 +2,12 @@ local lib, dir
 for path in package.cpath:gmatch('[^;]+') do
     lib = path:gsub('%?', '_ffilupa')
     if package.loadlib(lib, '*') then
-        dir = path:gsub('/[^/]*$', '/')
+        dir = path:gsub('/[^/]*$', '')
         break
     end
 end
 if dir == nil then
-    os.error('ffilupa lib not found')
+    error('ffilupa embedding library is not found (maybe installation is incorrect)')
 end
 local python_backup = _G.python
 local pypkg_backup = package.loaded['python']
@@ -15,4 +15,10 @@ package.loadlib(lib, 'ffilupa_init')(dir)
 local ffilupa = _G.python
 _G.python = python_backup
 package.loaded['python'] = pypkg_backup
+if ffilupa == nil then
+    error('python error during loading')
+end
+-- add debug info
+ffilupa._path = dir
+ffilupa._lib = lib
 return ffilupa
