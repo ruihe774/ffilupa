66d63a3c33dc768284e7c44d37d062c3653c338c
TitanSnow <sweeto@live.cn>
2018-04-20 18:56:28 +0800

    fallback to get __version__ from pkg_resources

--- ffilupa/__init__.py
+++ ffilupa/__init__.py
@@ -1,8 +1,12 @@
 def read_version():
     from os import path
     global __version__
-    with open(path.join(path.dirname(__file__), 'version.txt')) as f:
-        __version__ = f.read().rstrip()
+    try:
+        with open(path.join(path.dirname(__file__), 'version.txt')) as f:
+            __version__ = f.read().rstrip()
+    except OSError:
+        import pkg_resources
+        __version__ = pkg_resources.get_distribution(__package__).version
 read_version(); del read_version
 
 from .runtime import *
@@ -19,4 +23,4 @@ def _gen_all():
     from . import compat as _cp
     from . import lualibs as _ll
     __all__ = _rt.__all__ + _exc.__all__ + _prc.__all__ + _cp.__all__ + _ll.__all__
-_gen_all()
+_gen_all(); del _gen_all
