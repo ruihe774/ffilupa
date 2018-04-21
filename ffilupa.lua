d8a9231e94a877fa059a341088020bdeecbdea19
TitanSnow <sweeto@live.cn>
2018-04-21 10:13:41 +0800

    use package.searchpath

--- ffilupa.lua
+++ ffilupa.lua
@@ -1,17 +1,12 @@
-local lib, dir
-for path in package.cpath:gmatch('[^;]+') do
-    lib = path:gsub('%?', '_ffilupa')
-    if package.loadlib(lib, '*') then
-        dir = path:gsub('/[^/]*$', '')
-        break
-    end
+local lib, err = package.searchpath('_ffilupa', package.cpath)
+if lib == nil then
+    error('ffilupa embedding library is not found (maybe installation is incorrect)' .. err)
 end
-if dir == nil then
-    error('ffilupa embedding library is not found (maybe installation is incorrect)')
-end
+local path = lib:sub(1, lib:len() - lib:reverse():find(package.config:sub(1, 1), 1, true))
 local python_backup = _G.python
 local pypkg_backup = package.loaded['python']
-package.loadlib(lib, 'ffilupa_init')(dir)
+package.loadlib(lib, '*')
+package.loadlib(lib, 'ffilupa_init')(path)
 local ffilupa = _G.python
 _G.python = python_backup
 package.loaded['python'] = pypkg_backup
@@ -19,6 +14,6 @@ if ffilupa == nil then
     error('python error during loading')
 end
 -- add debug info
-ffilupa._path = dir
+ffilupa._path = path
 ffilupa._lib = lib
 return ffilupa
