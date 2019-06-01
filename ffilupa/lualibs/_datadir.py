bb826dc5c0038610660597461437143647a7b812
TitanSnow <sweeto@live.cn>
2019-06-01 15:17:21 +0800

    support posix

--- ffilupa/lualibs/_datadir.py
+++ ffilupa/lualibs/_datadir.py
@@ -12,8 +12,15 @@ def get_nt_data_dir_prefix() -> Path:
         raise ValueError('%APPDATA% is not set') from e
 
 
+def is_root() -> bool:
+    return os.geteuid() == 0
+
+
 def get_posix_data_dir_prefix() -> Path:
-    raise NotImplementedError
+    if is_root():
+        return Path(os.environ.get('XDG_DATA_DIRS', '/usr/local/share/:/usr/share/').split(':')[0])
+    else:
+        return Path(os.environ.get('XDG_DATA_HOME', os.path.expanduser('~/.local/share')))
 
 
 def get_data_dir_prefix() -> Path:
